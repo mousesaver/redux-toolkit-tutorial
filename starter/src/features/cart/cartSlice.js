8af002837fa6d93fc514a50a009be66338d2345c
mousesaver <zfan45@gmail.com>
2023-01-07 12:53:59 -0800

    cartContainer

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,8 +1,8 @@
 import  { createSlice } from "@reduxjs/toolkit"
-
+import cartItems from "../../cartItems"
 const initialState = {
-    cartItems: [],
-    amount: 1,
+    cartItems: cartItems,
+    amount: 4,
     total: 0,
     isLoading: true
 }
